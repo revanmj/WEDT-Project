0aad71ee316bdb1b29bcb615bf8943e37016f842
Michał Jakubowski <revanmj@gmail.com>
2015-04-11 17:37:35 +0200

    Wyszukiwanie i pobieranie tweetow

---
+++ src/wedt/project/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wedt.project;
+
+/**
+ *
+ * @author revanmj
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainWindow frame = new MainWindow();
+        frame.setVisible(true);
+    }
+    
+}
